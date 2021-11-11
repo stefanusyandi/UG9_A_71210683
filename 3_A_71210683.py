3897b1f8d234531d830d5746b5ecb10da8094358
stefanusyandi <93316047+stefanusyandi@users.noreply.github.com>
2021-11-11 21:58:57 +0700

    soal 3

---
+++ 3_A_71210683.py
@@ -0,0 +1,11 @@
+#item catatan 
+#Data
+itemA = input("Masukkan daftar belanja 1: ")
+itemB = input("Masukkan daftar belanja 2: ")
+itemtambah1 = input("Tambhkan data ke daftar belanja 1: ")
+itemtambah2 = input("Tambhkan data ke daftar belanja 2: ")
+#Proses hasil tambahan item
+print("Daftar belanja 1 adalah", itemA +","+itemtambah1)
+print("Daftar belanja 2 adalah", itemB +","+itemtambah2)
+
+
